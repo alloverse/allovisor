857defe3e0cb312e3700b712808871c8283680e3
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2019-10-29 23:08:34 +0100

    didn’t help

--- build-android.sh
+++ build-android.sh
@@ -5,7 +5,7 @@ echo ANDROID_HOME is $ANDROID_HOME
 
 echo PATH is $PATH
 
-export PATH="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin":~/Library/Android/sdk/platform-tools:$PATH 
+export PATH="/Users/runner/Library/Android/sdk/cmake/3.6.4111459/bin:/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin":~/Library/Android/sdk/platform-tools:$PATH 
 export ANDROID_HOME=${ANDROID_HOME:-~/Library/Android/sdk}
 export VISORSRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export APPSRC=$VISORSRC/deps/lovr-android
