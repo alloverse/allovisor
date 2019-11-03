ac0d041aeb5bc4993e366500c28ab1d7aa50080b
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2019-11-03 21:32:30 +0100

    draw rotations
    man I’m not in love with euler angles right now

--- lua/app/network/main.lua
+++ lua/app/network/main.lua
@@ -53,13 +53,48 @@ function NetworkScene:onDraw()
     if trans ~= nil and geom ~= nil then
       if geom.type == "hardcoded-model" then
         lovr.graphics.setColor(0, 0, 0)
-        lovr.graphics.cube('fill', trans.position.x, trans.position.y, trans.position.z, 1, 0, 0, 0, 0)
+        lovr.graphics.cube('fill', 
+          trans.position.x, trans.position.y, trans.position.z, 
+          1, 
+          euler2axisangle(trans.rotation.x, trans.rotation.y, trans.rotation.z)
+      )
       elseif geom.type == "inline" then
         
       end
     end
   end
 
+end
+
+-- https://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToAngle/
+function euler2axisangle(pitch, yaw, roll)
+	local c1 = math.cos(yaw/2)
+	local s1 = math.sin(yaw/2)
+	local c2 = math.cos(pitch/2)
+	local s2 = math.sin(pitch/2)
+	local c3 = math.cos(roll/2)
+	local s3 = math.sin(roll/2)
+	local c1c2 = c1*c2
+	local s1s2 = s1*s2
+	w =c1c2*c3 - s1s2*s3
+	x =c1c2*s3 + s1s2*c3
+	y =s1*c2*c3 + c1*s2*s3
+	z =c1*s2*c3 - s1*c2*s3
+	local angle = 2 * math.acos(w)
+	local norm = x*x+y*y+z*z
+  if norm < 0.001 then 
+    -- when all euler angles are zero angle =0 so
+		-- we can set axis to anything to avoid divide by zero
+		x=1
+    y=0
+    z=0
+	else
+		norm = math.sqrt(norm)
+    x = x / norm;
+    y = y / norm;
+    z = z / norm;
+  end
+  return angle, x, y, z
 end
 
 
