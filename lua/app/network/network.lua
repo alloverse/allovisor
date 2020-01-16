80158fd5ccde858e1e1ed2bc93766f87712bd4e6
Tobias Kask <tobias.kask@gmail.com>
2020-01-16 21:19:42 +0100

    Silly, early raycasting from the left hand.
    Only taking position into account, though - not rotation.

--- lua/app/network/network.lua
+++ lua/app/network/network.lua
@@ -17,7 +17,8 @@ if os == "Windows" then
   pkg, err = package.loadlib(dllpath, "luaopen_liballonet")
 elseif os == "macOS" or os == "Android" then
   print("loading liballonet from exe...")
-  pkg, err = package.loadlib(lovr.filesystem.getExecutablePath(), "luaopen_liballonet")
+  local manualpath = "/Users/kask/Alloverse/allovisor-lovr/build/Alloverse.app/Contents/MacOS/lovr"
+  pkg, err = package.loadlib(manualpath, "luaopen_liballonet")
 else
   error("don't know how to load allonet")
 end
