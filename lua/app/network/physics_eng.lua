407983eaa56df95ead09050eb1ee580cdde11272
Tobias Kask <tobias.kask@gmail.com>
2020-02-06 16:16:56 +0100

    implemented dummy onComponentAdded method to the physics engine

--- lua/app/network/physics_eng.lua
+++ lua/app/network/physics_eng.lua
@@ -7,6 +7,8 @@ local PhysicsEng = classNamed("PhysicsEng", Ent)
 
 function PhysicsEng:_init()
   self:super()
+
+  self.colliders = {}
 end
 
 function PhysicsEng:onLoad()
@@ -15,7 +17,33 @@ end
 
 function PhysicsEng:onUpdate(dt)
   
+  for i, collider in ipairs(self.colliders) do
+    local entity = collider.entity
+    local matrix = entity.components.transform:getMatrix()
+
+    local position = matrix:mul(lovr.math.vec3())
+
+    collider:setPosition(position:unpack())
+  end
 
 end
+
+function PhysicsEng:onComponentAdded(component_key, component)
+  
+  if component_key ~= "collider" then
+    return
+  end
+
+  local collider = self.world:newBoxCollider(0, 0, 0, component.width, component.height, component.depth)
+
+  local entity = component:getEntity()
+
+  entity.collider = collider
+  collider.entity = entity
+
+  table.insert(self.colliders, collider)
+
+end
+
 
 return PhysicsEng
