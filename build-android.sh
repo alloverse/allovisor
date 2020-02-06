aa4fe82b84b23dcde8133839738e9be34db74a5f
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-02-06 21:40:56 +0100

    better android cmake hardcode

--- build-android.sh
+++ build-android.sh
@@ -5,7 +5,7 @@ echo ANDROID_HOME is $ANDROID_HOME
 
 echo PATH is $PATH
 
-export PATH="/usr/local/Cellar/cmake/3.16.2/bin:/Users/runner/Library/Android/sdk/cmake/3.6.4111459/bin:/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin":~/Library/Android/sdk/platform-tools:$PATH 
+export PATH="/usr/local/bin:/usr/local/Cellar/cmake/3.16.3/bin:/Users/runner/Library/Android/sdk/cmake/3.6.4111459/bin:/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin":~/Library/Android/sdk/platform-tools:$PATH 
 export ANDROID_HOME=${ANDROID_HOME:-~/Library/Android/sdk}
 export VISORSRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export APPSRC=$VISORSRC/deps/lovr-android
