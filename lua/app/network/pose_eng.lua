2e9ea5eb8a357fcfa213feecde9c1707802825c9
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-02-23 23:19:11 +0100

    Cleanup pose engine

--- lua/app/network/pose_eng.lua
+++ lua/app/network/pose_eng.lua
@@ -7,7 +7,9 @@ local Entity, componentClasses = unpack(require("app.network.entity"))
 local PoseEng = classNamed("PoseEng", Ent)
 function PoseEng:_init()
   self.yaw = 0.0
-  
+  self.hoveredEntity = nil
+  self.pokedEntity = nil
+
   self:super()
 end
 
@@ -23,6 +25,11 @@ function pose2matrix(x, y, z, angle, ax, ay, az)
 end
 
 function PoseEng:onUpdate(dt)
+  self:updateIntent()
+  self:updatePointing()
+end
+
+function PoseEng:updateIntent()
   local mx, my = lovr.headset.getAxis("hand/left", "thumbstick")
   local tx, ty = lovr.headset.getAxis("hand/right", "thumbstick")
   self.yaw = self.yaw - (tx/30.0)
@@ -40,8 +47,9 @@ function PoseEng:onUpdate(dt)
   end
   
   self.parent.client:set_intent(intent)
+end
 
-
+function PoseEng:updatePointing()
   -- Find the left hand whose parent is my avatar and whose pose is left hand
   local lefthand_id = tablex.find_if(self.parent.state.entities, function(entity)
     return entity.components.relationships ~= nil and
@@ -50,42 +58,50 @@ function PoseEng:onUpdate(dt)
            entity.components.intent.actuate_pose == "hand/left"
   end)
   
-  if (lefthand_id ~= nil) then
-    local lefthand = self.parent.state.entities[lefthand_id]
-    if (lefthand ~= nil) then
-      local handPos = lefthand.components.transform:getMatrix():mul(lovr.math.vec3())
-      local distantPoint = lefthand.components.transform:getMatrix():mul(lovr.math.vec3(0,0,-10))      
+  if lefthand_id == nil then return end
+  
+  local lefthand = self.parent.state.entities[lefthand_id]
+  if lefthand == nil then return end
 
-      -- Raycast from the left hand
-      self.parent.physics.world:raycast(handPos.x, handPos.y, handPos.z, distantPoint.x, distantPoint.y, distantPoint.z, function(shape)
-       
-        -- TODO: Set the correct point of intersection (i.e. not just the "distantPoint")
-        -- self.parent:sendInteraction({
-        --   type = "one-way",
-        --   receiver_entity_id = shape:getCollider():getUserData().id,
-        --   body = {"point", {handPos.x, handPos.y, handPos.z}, {distantPoint.x, distantPoint.y, distantPoint.z}}
-        -- })
+  local handPos = lefthand.components.transform:getMatrix():mul(lovr.math.vec3())
+  local distantPoint = lefthand.components.transform:getMatrix():mul(lovr.math.vec3(0,0,-10))
 
-        if lovr.headset.wasPressed("hand/left", "trigger") then
-          self.parent:sendInteraction({
-            type = "request",
-            receiver_entity_id = shape:getCollider():getUserData().id,
-            body = {"poke", true}
-          })
-        end
+  self.hoveredEntity = nil
 
-        if lovr.headset.wasReleased("hand/left", "trigger") then
-          self.parent:sendInteraction({
-            type = "request",
-            receiver_entity_id = shape:getCollider():getUserData().id,
-            body = {"poke", false}
-          })
-        end
+  -- Raycast from the left hand
+  self.parent.physics.world:raycast(handPos.x, handPos.y, handPos.z, distantPoint.x, distantPoint.y, distantPoint.z, function(shape, hx, hy, hz)
+    -- assuming first hit is nearest; skip all other hovered entities.
+    if self.hoveredEntity == nil then
+      self.hoveredEntity = shape:getCollider():getUserData()
+    end
+  end)
 
-      end)
+  if self.hoveredEntity then
+    -- todo: server needs to be more resilient before we can start spamming these :S
+    -- self.parent:sendInteraction({
+    --   type = "one-way",
+    --   receiver_entity_id = shape:getCollider():getUserData().id,
+    --   body = {"point", {handPos.x, handPos.y, handPos.z}, {hx, hy, hz}}
+    -- })
+
+    if lovr.headset.wasPressed("hand/left", "trigger") then
+      self.pokedEntity = self.hoveredEntity
+      self.parent:sendInteraction({
+        type = "request",
+        receiver_entity_id = self.pokedEntity.id,
+        body = {"poke", true}
+      })
     end
   end
 
+  if self.pokedEntity and lovr.headset.wasReleased("hand/left", "trigger") then
+    self.parent:sendInteraction({
+      type = "request",
+      receiver_entity_id = self.pokedEntity.id,
+      body = {"poke", false}
+    })
+    self.pokedEntity = nil
+  end
 end
 
 return PoseEng
