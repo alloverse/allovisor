f48bd77806b310e49992e2d6a4296ef5c86886de
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-03-27 08:28:13 +0100

    put argv parsing in again so we can use lodr

--- src/allonet_loader.c
+++ src/allonet_loader.c
@@ -21,12 +21,79 @@ int main(int argc, char** argv)
     luax_setmainthread(L);
     luaL_openlibs(L);
 
+
+
     // arg table
+    // Args follow the lua standard https://en.wikibooks.org/wiki/Lua_Programming/command_line_parameter
+    // In this standard, the contents of argv are put in a global table named "arg", but with
+    // indices offset such that the "script" (in lovr, the game path) is at index 0. So all
+    // arguments (if any) intended for the script/game are at successive indices starting with 1,
+    // and the exe and its arguments are in normal order but stored in negative indices.
+    //
+    // LÖVR can be run in ways normal Lua can't. It can be run with an empty argv, or with no script
+    // (if fused).  So in the case of LÖVR:
+    // * The script path will always be at index 0, but it can be nil.
+    // * For a fused executable, whatever is given in argv as script name is still at 0, but will be ignored.
+    // * The exe (argv[0]) will always be the lowest-integer index. If it's present, it will always be -1 or less.
+    // * Any named arguments parsed by Lovr will be stored in the arg table at their named keys.
+    // * An exe name will always be stored in the arg table at string key "exe", even if none was supplied in argv.
     lua_newtable(L);
-    // push dummy "lovr" 
+    // push dummy "lovr" in case argv is empty
     lua_pushliteral(L, "lovr");
     lua_setfield(L, -2, "exe");
+
+    typedef enum { // What flag is being searched for?
+      ARGFLAG_NONE, // Not processing a flag
+      ARGFLAG_ROOT
+    } ArgFlag;
+
+    printf("Parsing %d args\n", argc);
+
+    ArgFlag currentFlag = ARGFLAG_NONE;
+    int lovrArgs = 0; // Arg count up to but not including the game path
+    // One pass to parse flags
+    for (int i = 0; i < argc; i++) {
+      printf("arg %d: %s\n", i, argv[i]);
+      if (lovrArgs > 0) {
+        // This argument is an argument to a -- flag
+        if (currentFlag == ARGFLAG_ROOT) {
+          lua_pushstring(L, argv[i]);
+          lua_setfield(L, -2, "root");
+          currentFlag = ARGFLAG_NONE;
+
+          // This argument is a -- flag
+        }
+        else if (!strcmp(argv[i], "--root") || !strcmp(argv[i], "-r")) {
+          currentFlag = ARGFLAG_ROOT;
+
+        }
+        else if (!strcmp(argv[i], "--console")) {
+          lovrPlatformOpenConsole();
+
+          // This is the game path
+        }
+        else {
+          break;
+        }
+      }
+      else { // Found exe name
+        lua_pushstring(L, argv[i]);
+        lua_setfield(L, -2, "exe");
+      }
+
+      lovrArgs++;
+    }
+    // Now that we know the negative offset to start at, copy all args in the table
+    for (int i = 0; i < argc; i++) {
+      lua_pushstring(L, argv[i]);
+      lua_rawseti(L, -2, -lovrArgs + i);
+    }
+
     lua_setglobal(L, "arg");
+
+
+
+
 
     lua_getglobal(L, "package");
     lua_getfield(L, -1, "preload");
