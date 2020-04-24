b1cf7f052c8395489fe44a3e78e889ca2a8524b4
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-04-24 22:15:17 +0200

    torso in menu

--- lua/app/menu/main_menu_scene.lua
+++ lua/app/menu/main_menu_scene.lua
@@ -20,7 +20,8 @@ function MainMenuScene:onLoad()
   self.models = {
     head = lovr.graphics.newModel('assets/models/mask/mask.glb'),
     lefthand = lovr.graphics.newModel('assets/models/left-hand/left-hand.glb'),
-    righthand = lovr.graphics.newModel('assets/models/right-hand/right-hand.glb')
+    righthand = lovr.graphics.newModel('assets/models/right-hand/right-hand.glb'),
+    torso = lovr.graphics.newModel('assets/models/torso/torso.glb')
   }
 end
 
@@ -28,6 +29,7 @@ function MainMenuScene:onDraw()
   MenuScene.onDraw(self)
   lovr.graphics.setColor({1,1,1})
   self.models.head:draw(     -1.5, 1.8, -1.2, 1.0, 3.14, 0, 1, 0, 1)
+  self.models.torso:draw(     -1.5, 1.2, -1.2, 1.0, 3.14, 0, 1, 0, 1)
   self.models.lefthand:draw( -1.3, 1.2, -1.2, 1.0, 3.14, -0.5, 1, 0, 1)
   self.models.righthand:draw(-1.8, 0.9, -1.4, 1.0, 3.14, 0.5, 1, 1, 1)
 
