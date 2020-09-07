4d3010d0fd075128004010a18ec9ccbc3bbed9fa
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-09-07 09:06:36 +0200

    --desktop CLI arg to run without VR

--- lua/conf.lua
+++ lua/conf.lua
@@ -1,4 +1,19 @@
+function poparg(needle)
+  local argn = #arg
+  for j=1, argn do
+    if arg[j] == needle then
+      for i=j,argn do
+        arg[i] = arg[i+1]
+      end
+      return true
+    end
+  end
+  return false
+end
 
 function lovr.conf(t)
-	--t.headset.drivers = {"desktop"}
+  -- Pass --desktop at startup (after asset path) to force desktop/fake driver
+  if poparg("--desktop") then
+    t.headset.drivers = {"desktop"}
+  end
 end
