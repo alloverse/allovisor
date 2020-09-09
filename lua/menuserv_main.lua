decbf38fd38bc80bdb1ac5e55a3a1c7fdd32e2d6
Nevyn Bengtsson <nevyn@alloverse.com>
2020-09-09 16:55:20 +0200

    Use the new polling api for menuserv

--- lua/menuserv_main.lua
+++ lua/menuserv_main.lua
@@ -6,12 +6,14 @@ local allonet = util.load_allonet()
 print("starting menu server", allonet, allonet.standalone_server_count())
 
 if allonet.standalone_server_count() == 0 then
-  local success = allonet.start_standalone_server(21338)
-  if success == false then
-    print("failed to start menu server.")
-  else
-    print("menu server shutting down", allonet)
+  local allosocket = allonet.start_standalone_server(21338)
+
+  local running = true
+  while running do
+    print("poll")
+    running = allonet.poll_standalone_server(allosocket)
   end
+  print("menu server shutting down", allonet)
 else
   print("menu server already running")
 end
