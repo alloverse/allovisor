7b08816bc82cdf8b223887d77ded198e34bc5c72
Nevyn Bengtsson <nevyn@alloverse.com>
2020-09-10 08:51:16 +0200

    update to latest lovr.run from bootstrap to get restat event

--- lua/main.lua
+++ lua/main.lua
@@ -86,30 +86,43 @@ end
 -- need a custom lovr.run to disable built-in lovr.audio.setPose
 function lovr.run()
   lovr.timer.step()
-  lovr.load()
+  if lovr.load then lovr.load(arg) end
   return function()
     lovr.event.pump()
     for name, a, b, c, d in lovr.event.poll() do
-      if name == 'quit' and (not lovr.quit or not lovr.quit()) then
+      if name == 'restart' then
+        local cookie = lovr.restart and lovr.restart()
+        return 'restart', cookie
+      elseif name == 'quit' and (not lovr.quit or not lovr.quit(a)) then
         return a or 0
       end
       if lovr.handlers[name] then lovr.handlers[name](a, b, c, d) end
     end
     local dt = lovr.timer.step()
+    if lovr.headset then
     lovr.headset.update(dt)
-    lovr.update(dt)
-
+    end
 	if lovr.audio then
+      lovr.audio.update()
+      if lovr.headset then
 	    lovr.audio.setVelocity(lovr.headset.getVelocity())
-		lovr.audio.update()
+      end
 	end
-
+    if lovr.update then lovr.update(dt) end
+    if lovr.graphics then
     lovr.graphics.origin()
+      if lovr.draw then
+        if lovr.headset then
     lovr.headset.renderTo(lovr.draw)
+        end
     if lovr.graphics.hasWindow() then
       lovr.mirror()
     end
+      end
     lovr.graphics.present()
+    end
+    if lovr.math then
 		lovr.math.drain()
   end
+end
 end
