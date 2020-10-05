b6b2542f3db5a66f247175634595d171ec3624df
Tobias Kask <tobias.kask@gmail.com>
2020-10-05 11:11:25 +0200

    Overlay improvements, added mouse

--- lua/app/test/controlsOverlay.lua
+++ lua/app/test/controlsOverlay.lua
@@ -9,50 +9,55 @@ local ControlsOverlay = classNamed("ControlsOverlay", ui2.ScreenEnt)
 
 function ControlsOverlay:onLoad()
 
-  print("=================")
-  print("= In the ControlsOverlay =")
-  print("=================")
-  
   ui2.routeMouse()
 
   local ents = {
 
     -- WASD buttons
-    ui2.AlloKeyEnt{label="W", onButton = function()
+    ui2.AlloKeyEnt{id="w", label="W", onButton = function()
       self:onKeyPress("w")
     end},
-    ui2.AlloKeyEnt{label="A", onButton = function()
+    ui2.AlloKeyEnt{id="a",label="A", onButton = function()
       self:onKeyPress("a")
     end},
-    ui2.AlloKeyEnt{label="S", onButton = function()
+    ui2.AlloKeyEnt{id="s",label="S", onButton = function()
       self:onKeyPress("s")
     end},
-    ui2.AlloKeyEnt{label="D", onButton = function()
+    ui2.AlloKeyEnt{id="d",label="D", onButton = function()
       self:onKeyPress("d")
     end},
-    ui2.AlloLabelUiEnt{label="Move"},
+    ui2.AlloLabelUiEnt{id="movelabel", label="Move"},
 
     
     -- Close button
-    ui2.AlloKeyEnt{label="esc", onButton = function()
+    ui2.AlloKeyEnt{id="escape", label="esc", onButton = function()
       print("Closing control overlay")
       self:onKeyPress("escape")
     end},
-    ui2.AlloLabelUiEnt{label="Close"},
+    ui2.AlloLabelUiEnt{id="closelabel", label="Close"},
 
 
     -- Grab button
-    ui2.AlloKeyEnt{label="F", onButton = function()
+    ui2.AlloKeyEnt{id="f", label="F", onButton = function()
       self:onKeyPress("f")
     end},
-    ui2.AlloLabelUiEnt{label="Grab"},
+    ui2.AlloLabelUiEnt{id="grablabel", label="Grab"},
 
 
+    -- Mouse
+    ui2.AlloMouseButtonEnt{id="lmb", label="", onButton = function()
+      self:onClickDown()
+    end},
+    ui2.AlloMouseButtonEnt{id="rmb", label="", onButton = function()
+      self:onClickDown()
+    end},
+
+    ui2.AlloLabelUiEnt{id="lookLabel", label="Look"},
+    ui2.AlloLabelUiEnt{id="interactLabel", label="Interact"},
+    ui2.AlloLabelUiEnt{id="pointLabel", label="Point"},
+
+    ui2.AlloDummyMouseEnt{id="dummyMouse"},
   }
-
-  -- local logo = ui.Surface(ui.Bounds(-0.65, 0.8, 0.01, 0.2, 0.2, 0.2))
-  -- logo:setTexture("iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAB/UlEQVR4nO2av27CMBCHL0AhQqD+GdshUtVnqNStQxk6dUHqVhbExtTn4MX6Bn2CPsZ1wZEJcRLbd7ZD7ifdwJL4++4sRyQAEolEIpFQBY/V1+s7BwEAVwfE1QHVIikXipMR6NdNRsIJuF5As1i8zUcIAPi3fy/rZ/MaXUS5gDp4gwine+jg1YLQE1EURWdwDxGt4LFEOIFbbAtcXGXW8AYR9OC+8C0ivMC5RJCDN4mggqfaFkHAd18fuN+u2SS4iCiPHS5wdZ7vt+uzCiQibtfrwKsS6s5+zmnAmxlv17uAx5wGVvDlYm4Nr+qxuMfrGfs00ArQx8wVPMQ0sAigBucUQSqAG7wqYTr2e2IkE8Ax7qGmwVtALHAqEc4CUgGvk2AjwktAbGCKSRABlyZASRABIkAEiIAu9fJwN2wBg5+A7+enYQvQpkAEWP0dJgJEgAiIDioCDLWcZk7PAaD96LWA23zUCVy9eIGatEpIWUDTBPzu3s663hSjiJQFmCbABrxVRMoC8klGBn4iQX9jnLIANQHQss+dRaiKDdokAIi63jsRIcCNMhIBDw5fSsgnWXARAIDz431jgVcTTAJE7nhb2ERA4uB6cDam2xbAdKyFiPeJAT3qelOsJcCFgFfTKmLzyffNbyopP3EZStdNKSXAwMD1DBZcIpFIksg/LCx2xNxTGTMAAAAASUVORK5CYII=")
-  -- table.insert(ents, logo)
 
   local alloCustomLayout = ui2.AlloCustomLayout{managed=ents, parent=self, pass={swap=self}}
   alloCustomLayout:layout()
@@ -64,6 +69,10 @@ function ControlsOverlay:onKeyPress(code, scancode)
   if code == "escape" then
     self:die()
   end
+end
+
+function ControlsOverlay:onMousePress(x, y)
+  print("ControlsOverlay:onMousePress", x, y)
 end
 
 function ControlsOverlay:onMirror()
--- lua/ent/ui2/init.lua
+++ lua/ent/ui2/init.lua
@@ -205,6 +205,7 @@ function ui2.AlloCustomLayout:layout(relayout)
 	local xface = self.face == "x"
 	local axis = vec2(moveright and 1 or -1, moveup and 1 or -1) -- Only thing anchor impacts
 
+    
 
 	for i = startAt,mn do -- Lay out everything not laid out
     local e = self.managed[i] -- Entity to place
@@ -214,49 +215,56 @@ function ui2.AlloCustomLayout:layout(relayout)
     local labelHeight = 0.1
     local labelSpacing = 0.02 -- vertical spacing between label and button
 
-    local mouseButtonWidth = 0.15
-    local mouseButtonHeight = 0.3
+    local mouseButtonWidth = 0.1
+    local mouseButtonHeight = 0.15
+
+    local dummyMouseWidth = 0.2
+    local dummyMouseHeight = 0.3
+
+    local dummyMouseRightMargin = 0.3
 
     local bound
-    if e.label == "W" then
-      print("layouting W")
-      bound = bound2(vec2(leftedge+spacing+buttonWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing+spacing*2+buttonHeight), vec2(leftedge+spacing+buttonWidth+0.2, bottomedge-labelHeight/2+labelHeight+labelSpacing+spacing*3+buttonHeight*2))
-    elseif e.label == "A" then
-      print("layouting A")
+
+    -- wasd
+    if e.id == "w" then
+      bound = bound2(vec2(leftedge+spacing+buttonWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing+spacing*2+buttonHeight), vec2(leftedge+spacing+buttonWidth*2, bottomedge-labelHeight/2+labelHeight+labelSpacing+spacing*3+buttonHeight*2))
+    elseif e.id == "a" then
       bound = bound2(vec2(leftedge, bottomedge-labelHeight/2+labelHeight+labelSpacing+spacing), vec2(leftedge+buttonWidth, bottomedge-labelHeight/2+spacing+labelHeight+labelSpacing+buttonHeight))
-    elseif e.label == "S" then
-      print("layouting S")
+    elseif e.id == "s" then
       bound = bound2(vec2(leftedge+spacing+buttonWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing+spacing), vec2(leftedge+spacing+buttonWidth*2, bottomedge-labelHeight/2+spacing+labelHeight+labelSpacing+buttonHeight))  
-    elseif e.label == "D" then
-      print("layouting D")
+    elseif e.id == "d" then
       bound = bound2(vec2(leftedge+spacing*2+buttonWidth*2, bottomedge-labelHeight/2+labelHeight+labelSpacing+spacing), vec2(leftedge+spacing*2+buttonWidth*3, bottomedge-labelHeight/2+spacing+labelHeight+labelSpacing+buttonHeight))
-    elseif e.label == "Move" then
-      print("layouting Move Label")
+    elseif e.id == "movelabel" then
       bound = bound2(vec2(leftedge+buttonWidth, bottomedge-labelHeight/2), vec2(leftedge+spacing+buttonWidth*2, bottomedge-labelHeight/2+labelHeight))
 
-    elseif e.label == "esc" then
-      print("layouting esc")
+    -- close
+    elseif e.id == "escape" then
       bound = bound2(vec2(leftedge, topedge-buttonHeight), vec2(leftedge+buttonWidth, topedge))
-    elseif e.label == "Close" then
-      print("layouting Close Label")
+    elseif e.id == "closelabel" then
       bound = bound2(vec2(leftedge, topedge-buttonHeight-labelHeight-labelSpacing-spacing), vec2(leftedge+buttonWidth, topedge-buttonHeight-labelSpacing-spacing))
 
-    elseif e.label == "F" then
-      print("layouting esc")
+    -- grab
+    elseif e.id == "f" then
       bound = bound2(vec2(.8+leftedge, bottomedge-labelHeight/2+labelHeight+labelSpacing), vec2(.8+leftedge+buttonWidth, bottomedge-labelHeight/2+buttonHeight+labelHeight+labelSpacing))
-    elseif e.label == "Grab" then
-      print("layouting Grab Label")
+    elseif e.id == "grablabel" then
       bound = bound2(vec2(.8+leftedge, bottomedge-labelHeight/2), vec2(.8+leftedge+buttonWidth, bottomedge-labelHeight/2+labelHeight))
 
-    elseif e.label == "lmb" then
-      print("layouting Left Mouse Button")
-      bound = bound2(vec2(rightedge-mouseButtonWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing), vec2(rightEdge, bottomedge-labelHeight/2+buttonHeight+labelHeight+labelSpacing))
-    elseif e.label == "rmb" then
-      print("layouting Right Mouse Button")
-      bound = bound2(vec2(rightedge-mouseButtonWidth, bottomedge), vec2(rightEdge, bottomedge-.3))
+    -- mouse
+    elseif e.id == "dummyMouse" then
+      bound = bound2(vec2(rightedge-dummyMouseRightMargin-dummyMouseWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing), vec2(rightedge-dummyMouseRightMargin, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight))
+    elseif e.id == "lmb" then
+      bound = bound2(vec2(rightedge-dummyMouseRightMargin-dummyMouseWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight-mouseButtonHeight), vec2(rightedge-dummyMouseRightMargin-dummyMouseWidth+mouseButtonWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight))
+    elseif e.id == "rmb" then
+      bound = bound2(vec2(rightedge-dummyMouseRightMargin-dummyMouseWidth+mouseButtonWidth, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight-mouseButtonHeight), vec2(rightedge-dummyMouseRightMargin, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight))
+    elseif e.id == "lookLabel" then
+      bound = bound2(vec2(rightedge-0.78, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight-labelHeight), vec2(rightedge-0.58, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight))
+    elseif e.id == "interactLabel" then
+      bound = bound2(vec2(rightedge-0.22, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight-labelHeight), vec2(rightedge-0.02, bottomedge-labelHeight/2+labelHeight+labelSpacing+dummyMouseHeight))
+    elseif e.id == "pointLabel" then
+      bound = bound2(vec2(rightedge-dummyMouseRightMargin-dummyMouseWidth, bottomedge-labelHeight/2), vec2(rightedge-dummyMouseRightMargin, bottomedge-labelHeight/2+labelHeight))
 
     else
-      print("layouting something else")
+      print("UNEXPECTED: Layouting something not strictly defined")
       bound = bound2(vec2(0,0), vec2(0.2,0.2))
     end
     e.bound = bound
@@ -281,13 +289,13 @@ function RouteMouseEnt:onLoad()
 		ent.root:route(key, vec2(inx, iny)) -- FIXME: Better routing?
 	end
 
-	lovr.handlers['mousepressed'] = function(x,y)
-		route("onPress", x, y)
-	end
+	-- lovr.handlers['mousepressed'] = function(x,y)
+	-- 	route("onPress", x, y)
+	-- end
 
-	lovr.handlers['mousereleased'] = function(x,y)
-		route("onRelease", x, y)
-  end
+	-- lovr.handlers['mousereleased'] = function(x,y)
+	-- 	route("onRelease", x, y)
+  -- end
 
 end
 
@@ -350,9 +358,32 @@ function ui2.UiEnt:onMirror()
 
 	lovr.graphics.setColor(1,1,1,1)
 	lovr.graphics.setFont(flat.font)
-	lovr.graphics.print(self.label, center.x, center.y, 0, flat.fontscale)
+  lovr.graphics.print(self.label, center.x, center.y, 0, flat.fontscale)
 end
 
+
+
+-- Draw dummy Mouse
+ui2.AlloDummyMouseEnt = classNamed("AlloDummyMouseEnt", ui2.UiBaseEnt)
+
+local dummyMouseTex = lovr.graphics.newTexture("assets/textures/controls-overlay/dummy-mouse.png", {})
+local dummyMouseMat = lovr.graphics.newMaterial(dummyMouseTex, 1, 1, 1, 1)
+
+
+function ui2.AlloDummyMouseEnt:sizeHint(margin, overrideText)
+  return vec2(0.2,0.3)
+end
+
+function ui2.AlloDummyMouseEnt:onMirror()
+  -- ui2.UiBaseEnt.onMirror(self)
+
+	local center = self.bound:center()
+  local size = self.bound:size()
+	
+  -- set dummyMouse texture (technically, material)
+  
+  lovr.graphics.plane(dummyMouseMat, center.x, center.y, 0, size.x, size.y)
+end
 
 
 
@@ -378,21 +409,22 @@ function ui2.AlloLabelUiEnt:onMirror()
 
   lovr.graphics.setColor(0,0,0,0.30)
   lovr.graphics.plane('fill', center.x, center.y, 0, size.x, size.y)
-  lovr.graphics.arc('fill', 'pie', center.x+size.x/2, center.y, 0, size.x/4, 0, 0, 1, 0, -math.pi/2, math.pi/2, 32)
-  lovr.graphics.arc('fill', 'pie', center.x-size.x/2, center.y, 0, size.x/4, 0, 0, 1, 0, math.pi/2, math.pi*1.5, 32)
+  lovr.graphics.arc('fill', 'pie', center.x+size.x/2, center.y, 0, size.y/2, 0, 0, 1, 0, -math.pi/2, math.pi/2, 32)
+  lovr.graphics.arc('fill', 'pie', center.x-size.x/2, center.y, 0, size.y/2, 0, 0, 1, 0, math.pi/2, math.pi*1.5, 32)
 
   lovr.graphics.setFont(flat.font)
   lovr.graphics.setColor(1,1,1,1)
-	lovr.graphics.print(self.label, center.x, center.y, 0, flat.fontscale)
+  lovr.graphics.print(self.label, center.x, center.y, 0, flat.fontscale)
+  lovr.graphics.setColor(1,1,1,1)
 end
 
 
 
 ui2.AlloKeyEnt = classNamed("AlloKeyEnt", ui2.UiBaseEnt) -- Expects in spec: bounds=, label=
 
-local whiteBtnTex = lovr.graphics.newTexture("assets/textures/key.png", {})
+local whiteBtnTex = lovr.graphics.newTexture("assets/textures/controls-overlay/key.png", {})
 local whiteBtnMat = lovr.graphics.newMaterial(whiteBtnTex, 1, 1, 1, 1)
-local whiteBtnDownTex = lovr.graphics.newTexture("assets/textures/key-down.png")
+local whiteBtnDownTex = lovr.graphics.newTexture("assets/textures/controls-overlay/key-down.png")
 local whiteBtnDownMat = lovr.graphics.newMaterial(whiteBtnDownTex, 1, 1, 1, 1)
 
 function ui2.AlloKeyEnt:onLoad()
@@ -404,15 +436,13 @@ function ui2.AlloKeyEnt:sizeHint(margin, overrideText)
 end
 
 function ui2.AlloKeyEnt:onKeyPress(code)
-  -- print("label:", string.lower(self.label))
-  -- print("code:", code)
-  if code == string.lower(self.label) then
+  if code == string.lower(self.id) then
     self.down = true
   end
 end
 
 function ui2.AlloKeyEnt:onKeyReleased(code)
-  if code == string.lower(self.label) then
+  if code == string.lower(self.id) then
     self.down = false
   end
 end
@@ -448,6 +478,61 @@ function ui2.AlloKeyEnt:onMirror()
   lovr.graphics.setFont(flat.font)
   lovr.graphics.setColor(buttonLabelColor,buttonLabelColor,buttonLabelColor,1)
   lovr.graphics.print(self.label, center.x, center.y, 0, flat.fontscale)
+  lovr.graphics.setColor(1,1,1,1)
+end
+
+
+
+ui2.AlloMouseButtonEnt = classNamed("AlloMouseButtonEnt", ui2.UiBaseEnt) -- Expects in spec: bounds=, label=
+
+local whiteLmbTex = lovr.graphics.newTexture("assets/textures/controls-overlay/lmb.png", {})
+local whiteLmbMat = lovr.graphics.newMaterial(whiteLmbTex, 1, 1, 1, 1)
+local whiteLmbDownTex = lovr.graphics.newTexture("assets/textures/controls-overlay/lmb-down.png")
+local whiteLmbDownMat = lovr.graphics.newMaterial(whiteLmbDownTex, 1, 1, 1, 1)
+
+local whiteRmbTex = lovr.graphics.newTexture("assets/textures/controls-overlay/rmb.png", {})
+local whiteRmbMat = lovr.graphics.newMaterial(whiteRmbTex, 1, 1, 1, 1)
+local whiteRmbDownTex = lovr.graphics.newTexture("assets/textures/controls-overlay/rmb-down.png")
+local whiteRmbDownMat = lovr.graphics.newMaterial(whiteRmbDownTex, 1, 1, 1, 1)
+
+function ui2.AlloMouseButtonEnt:onLoad()
+end
+
+function ui2.AlloMouseButtonEnt:sizeHint(margin, overrideText)
+  -- hardcodes AlloMouseButtonEnt size instead of dynamically setting it from text height & width
+  return vec2(0.1, 0.2)
+end
+
+function ui2.AlloMouseButtonEnt:onMousePressed(x, y, button)
+  -- print("AlloMouseButtonEnt:onMousePressed")
+  -- print("self.id", self.id)
+  -- print("button", button)
+  -- print("-------")
+--if self.id == "lmb" and button == 1 or self.id == "rmb" and button == 2 then
+    self.down = true
+--  end
+end 
+
+function ui2.AlloMouseButtonEnt:onMouseReleased(x, y, button)
+--  if self.id == "lmb" and button == 1 or self.id == "rmb" and button == 2 then
+    self.down = false
+--  end
+end
+
+function ui2.AlloMouseButtonEnt:onMirror()
+  ui2.UiEnt.onMirror(self)
+
+	local center = self.bound:center()
+  local size = self.bound:size()
+	
+  -- set button texture (technically, material) based on button being down or not
+  lovr.graphics.setColor(1,1,1,1)
+  if self.id == "lmb" then
+    lovr.graphics.plane(self.down and whiteLmbDownMat or whiteLmbMat, center.x, center.y, 0.1, size.x, size.y)
+  elseif self.id == "rmb" then
+    lovr.graphics.plane(self.down and whiteRmbDownMat or whiteRmbMat, center.x, center.y, 0.1, size.x, size.y)
+  end
+
 end
 
 
@@ -481,7 +566,8 @@ function ui2.ButtonEnt:onMirror()
 	local size = self.bound:size()
 	local gray = self.down and 0.5 or 0.8
 	lovr.graphics.setColor(gray,gray,gray,0.8)
-	lovr.graphics.plane('fill', center.x, center.y, 0, size.x, size.y)
+  lovr.graphics.plane('fill', center.x, center.y, 0, size.x, size.y)
+  lovr.graphics.setColor(1,1,1,1)
 
 	ui2.UiEnt.onMirror(self)
 end
