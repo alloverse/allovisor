b653f1be58534b6f185112da1a98b96e73f84827
Nevyn Bengtsson <nevyn@alloverse.com>
2020-10-19 15:07:09 +0200

    add a handtracking test
    So I can quickly test hand rigging for iuliana

--- lua/tests/handtracking.lua
+++ lua/tests/handtracking.lua
@@ -1,11 +1,41 @@
+function lovr.load()
+    leftHand = lovr.graphics.newModel("assets/models/avatars/female/left-hand.glb")
+    pbr = lovr.graphics.newShader(
+        'standard',
+        {
+            flags = {
+            normalMap = true,
+            indirectLighting = true,
+            occlusion = true,
+            emissive = true,
+            skipTonemap = false,
+            animated = true,
+            },
+            stereo = lovr.headset == nil or (lovr.headset.getName() ~= "Pico") -- turn off stereo on pico: it's not supported
+        }
+    )
+end
 
+function drawHand(hand)
+    lovr.graphics.setShader()
+    local x, y, z, a, ax, ay, az = lovr.headset.getPose(hand)
+    lovr.graphics.cube("line", x, y, z, 0.1, a, ax, ay, az)
+    for i, joint in ipairs(lovr.headset.getSkeleton(hand) or {}) do
+        local px, py, pz, pa, pax, pay, paz = unpack(joint)
+        lovr.graphics.points(unpack(joint, 1, 3))
+        --leftHand:pose(i, px, py, pz, pa, pax, pay, paz)
+    end
+
+    if hand == "hand/left" then
+        lovr.graphics.setShader(pbr)
+        leftHand:draw(x, y, z, 1.0, a, ax, ay, az)
+    end
+end
 function lovr.draw()
     lovr.graphics.cube('line', 0, 1.2, -3, .5, lovr.timer.getTime())
 
-    for _, hand in ipairs({ 'left', 'right' }) do
-      for _, joint in ipairs(lovr.headset.getSkeleton(hand) or {}) do
-        lovr.graphics.points(unpack(joint, 1, 3))
-      end
+    for _, hand in ipairs({ 'hand/left', 'hand/right' }) do
+        drawHand(hand)
     end
-  end
+end
   
