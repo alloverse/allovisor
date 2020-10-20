917bda58453d8c567dce596aaa315173b347a2f3
Nevyn Bengtsson <nevyn@alloverse.com>
2020-10-20 08:37:22 +0200

    tests/handtracking: print expected node names

--- lua/tests/handtracking.lua
+++ lua/tests/handtracking.lua
@@ -1,3 +1,32 @@
+local nodeNames = {
+    "palm",
+    "wrist",
+    "thumb_metacarpal",
+    "thumb_proximal",
+    "thumb_distal",
+    "thumb_tip",
+    "index_metacarpal",
+    "index_proximal",
+    "index_intermediate",
+    "index_distal",
+    "index_tip",
+    "middle_metacarpal",
+    "middle_proximal",
+    "middle_intermediate",
+    "middle_distal",
+    "middle_tip",
+    "ring_metacarpal",
+    "ring_proximal",
+    "ring_intermediate",
+    "ring_distal",
+    "ring_tip",
+    "little_metacarpal",
+    "little_proximal",
+    "little_intermediate",
+    "little_distal",
+    "little_tip",
+}
+
 function lovr.load()
     models = {
         ["hand/left"] = lovr.graphics.newModel("assets/models/avatars/female/left-hand.glb"),
@@ -6,12 +35,12 @@ function lovr.load()
         'standard',
         {
             flags = {
-            normalMap = true,
-            indirectLighting = true,
-            occlusion = true,
-            emissive = true,
-            skipTonemap = false,
-            animated = true,
+                normalMap = true,
+                indirectLighting = true,
+                occlusion = true,
+                emissive = true,
+                skipTonemap = false,
+                animated = true,
             },
             stereo = lovr.headset == nil or (lovr.headset.getName() ~= "Pico") -- turn off stereo on pico: it's not supported
         }
@@ -33,11 +62,16 @@ function drawHand(hand)
         local px, py, pz, pa, pax, pay, paz = unpack(joint)
         lovr.graphics.push()
         lovr.graphics.transform(px, py, pz, 1, 1, 1, pa, pax, pay, paz)
-        lovr.graphics.setColor(0.6, 0.2, 0.2)
+        lovr.graphics.setColor(0.6, 0.2, 0.2, 0.5)
         lovr.graphics.sphere(0, 0, 0, 0.01)
-        lovr.graphics.setColor(0.2,0,0)
+        lovr.graphics.setColor(0.6,0,0)
         lovr.graphics.transform(0, 0.03, 0.0, 1, 1, 1, -3.14/2, 1, 0, 0)
-        lovr.graphics.print(tostring(i-1), 0, 0, 0, 0.015)
+        lovr.graphics.print(tostring(i-1), 0, 0.005, 0, 0.007)
+        if models[hand] and i <= models[hand]:getNodeCount() then
+            lovr.graphics.print(tostring(models[hand]:getNodeName(i)), 0, 0.00, 0, 0.007)
+        end
+        lovr.graphics.setColor(0,0.6,0)
+        lovr.graphics.print(nodeNames[i], 0, -0.005, 0, 0.007)
         lovr.graphics.pop()
         --leftHand:pose(i, px, py, pz, pa, pax, pay, paz)
     end
