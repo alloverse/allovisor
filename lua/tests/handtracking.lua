415f38ca7ccc312e43e7db1a396980c73814c4f4
Nevyn Bengtsson <nevyn@alloverse.com>
2020-10-21 22:14:57 +0200

    more handtracking test code

--- lua/tests/handtracking.lua
+++ lua/tests/handtracking.lua
@@ -52,35 +52,83 @@ function lovr.load()
     lovr.graphics.setColor(0,0,0)
 end
 
+function drawAxes(size)
+    lovr.graphics.setColor(1,0,0)
+    lovr.graphics.line(0,0,0, size,0,0)
+    lovr.graphics.setColor(0,1,0)
+    lovr.graphics.line(0,0,0, 0,size,0)
+    lovr.graphics.setColor(0,0,1)
+    lovr.graphics.line(0,0,0, 0,0,size)
+end
+
 function drawHand(hand)
     models[hand] = models[hand] or lovr.headset.newModel(hand)
-    
+    local model = models[hand]
+    lovr.graphics.push()
+
     lovr.graphics.setShader()
-    local x, y, z, a, ax, ay, az = lovr.headset.getPose(hand)
-    lovr.graphics.cube("line", x, y, z, 0.1, a, ax, ay, az)
+
+    if model then
+        model:pose()
+    end
     for i, joint in ipairs(lovr.headset.getSkeleton(hand) or {}) do
-        local px, py, pz, pa, pax, pay, paz = unpack(joint)
-        lovr.graphics.push()
-        lovr.graphics.transform(px, py, pz, 1, 1, 1, pa, pax, pay, paz)
-        lovr.graphics.setColor(0.6, 0.2, 0.2, 0.5)
-        lovr.graphics.sphere(0, 0, 0, 0.01)
-        lovr.graphics.setColor(0.6,0,0)
-        lovr.graphics.transform(0, 0.03, 0.0, 1, 1, 1, -3.14/2, 1, 0, 0)
-        lovr.graphics.print(tostring(i-1), 0, 0.005, 0, 0.007)
-        if models[hand] and i <= models[hand]:getNodeCount() then
-            lovr.graphics.print(tostring(models[hand]:getNodeName(i)), 0, 0.00, 0, 0.007)
+        local x, y, z, a, ax, ay, az = unpack(joint)
+        local jointPose = lovr.math.mat4(unpack(joint))
+        local nodeName = nodeNames[i]
+        local status, ox, oy, oz, oa, oax, oay, oaz = pcall(model.getNodePose, model, nodeName, "local")
+        if status and hand == "hand/left" then
+            lovr.graphics.setColor(0,0.6,0)
+            model:pose(nodeName, ox, oy, oz, a, ax, ay, az)
+        else
+             lovr.graphics.setColor(0.6,0,0)
         end
-        lovr.graphics.setColor(0,0.6,0)
-        lovr.graphics.print(nodeNames[i], 0, -0.005, 0, 0.007)
+
+        lovr.graphics.push()
+
+        lovr.graphics.transform(jointPose)
+        lovr.graphics.sphere(0, 0, 0, 0.01)
+        drawAxes(0.018)
+        
+        lovr.graphics.transform(0, 0.03, 0.0, 1, 1, 1, -3.14/2, 1, 0, 0)
+        
+        
+        
+        if status then
+            lovr.graphics.print(string.format("%.2f %.2f %.2f", ox, oy, oz), 0, 0.008, 0, 0.007, lovr.math.quat(), 0, "left")
+        end
+        lovr.graphics.print(string.format("%.2f %.2f %.2f %.2f %.2f %.2f %.2f", x, y, z, a, ax, ay, az), 0, 0.004, 0, 0.007, lovr.math.quat(), 0, "left")
+        lovr.graphics.print(tostring(i-1), 0, 0.000, 0, 0.007, lovr.math.quat(), 0, "left")
+        lovr.graphics.print(nodeNames[i], 0, -0.004, 0, 0.007, lovr.math.quat(), 0, "left")
+        
         lovr.graphics.pop()
-        --leftHand:pose(i, px, py, pz, pa, pax, pay, paz)
+
+        if hand == "hand/left" then
+            lovr.graphics.setColor(0,0,0)
+            local h = 0.05
+            lovr.graphics.print(nodeNames[i], 0, i*h, -2, h, lovr.math.quat(), 0, "left")
+            lovr.graphics.print(string.format("(%.2f, %.2f, %.2f)", x, y, z), 0.5, i*h, -2, h, lovr.math.quat(), 0, "left")
+            lovr.graphics.print(string.format("%.2frad (%.2f, %.2f, %.2f)", a, ax, ay, az), 0.9, i*h, -2, h, lovr.math.quat(), 0, "left")
+            if status then
+                lovr.graphics.print(string.format("(%.2f, %.2f, %.2f)", ox, oy, oz), 1.5, i*h, -2, h, lovr.math.quat(), 0, "left")
+            end
+        end
     end
+
+    local pose = lovr.math.mat4(lovr.headset.getPose(hand))
+    lovr.graphics.transform(pose)
+    lovr.graphics.cube("line", 0,0,0, 0.1)
+    drawAxes(0.06)
+    
     lovr.graphics.setColor(1, 1, 1)
-    if models[hand] then
-        lovr.headset.animate(hand, models[hand])
+    if model then
+        if hand == "hand/right" then
+            lovr.headset.animate(hand, model)
+        end
         lovr.graphics.setShader(pbr)
-        models[hand]:draw(x, y, z, 1.0, a, ax, ay, az)
+        model:draw()
     end
+    lovr.graphics.pop()
+    
 end
 function lovr.draw()
     lovr.graphics.clear()
