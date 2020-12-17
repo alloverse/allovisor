19d1e86d7f7afd1ee7552beece364e5edfd07b85
Nevyn Bengtsson <nevyn@alloverse.com>
2020-12-17 15:07:23 +0100

    fix random seed issue

--- lua/lib/allostring.lua
+++ lua/lib/allostring.lua
@@ -5,15 +5,7 @@ for i = 48,  57 do table.insert(charset, string.char(i)) end
 for i = 65,  90 do table.insert(charset, string.char(i)) end
 for i = 97, 122 do table.insert(charset, string.char(i)) end
 
-function string.random(length)
-  if length > 0 then
-    return string.random(length - 1) .. charset[math.random(1, #charset)]
-  else
-    return ""
-  end
-end
-
-math.randomseed( os.time() )
+string.random = require("alloui.random_string")
 
 function string.has_suffix(s, suffix)
   return s:sub(-string.len(suffix)) == suffix
