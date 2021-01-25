46fdb1b5aef23b3ff7e68c7c442ebfbc764baf4b
Nevyn Bengtsson <nevyn@alloverse.com>
2021-01-25 17:09:58 +0100

    use a correct skybox

--- lua/shader/alloPbrShader.lua
+++ lua/shader/alloPbrShader.lua
@@ -15,18 +15,40 @@ local alloPbrShader = lovr.graphics.newShader(
 
 -- global, not local! leak this so it lives as long as the shader. 
 -- otherwise, it's deallocated before shader is used.
-environmentMap = lovr.graphics.newTexture({
-  left = 'assets/textures/skybox/skybox-left.jpg',
-  right = 'assets/textures/skybox/skybox-right.jpg',
-  top = 'assets/textures/skybox/skybox-top.jpg',
-  bottom = 'assets/textures/skybox/skybox-bottom.jpg',
-  back = 'assets/textures/skybox/skybox-back.jpg',
-  front = 'assets/textures/skybox/skybox-front.jpg'
-})
+skybox = lovr.graphics.newTexture({
+  left = 'assets/env/nx.png',
+  right = 'assets/env/px.png',
+  top = 'assets/env/py.png',
+  bottom = 'assets/env/ny.png',
+  back = 'assets/env/pz.png',
+  front = 'assets/env/nz.png'
+}, { linear = true })
+
+environmentMap = lovr.graphics.newTexture(256, 256, { type = 'cube' })
+for mipmap = 1, environmentMap:getMipmapCount() do
+  for face, dir in ipairs({ 'px', 'nx', 'py', 'ny', 'pz', 'nz' }) do
+    local filename = ('assets/env/m%d_%s.png'):format(mipmap - 1, dir)
+    local image = lovr.data.newTextureData(filename, false)
+    environmentMap:replacePixels(image, 0, 0, face, mipmap)
+  end
+end
+
+local sphericalHarmonics = {
+  { 0.375931705762083,  0.358656319030575,  0.308453761543167},
+  { 0.354636762547562,  0.351933427534857,  0.346643193593808},
+  { 0.213680467209818,  0.208764154253828,  0.185992646424045},
+  { 0.094452764555327,  0.087423796528460,  0.078299527458197},
+  { 0.129722100892800,  0.126512402478568,  0.121503396241859},
+  { 0.236712791137010,  0.233931621210715,  0.222236168254197},
+  {-0.020735133497873, -0.020241604203896, -0.018707677741929},
+  {-0.073279787762454, -0.073091939150411, -0.068554941014884},
+  {-0.102740171078351, -0.099605437423063, -0.094746265045717}
+}
 
 alloPbrShader:send('lovrLightDirection', { -1, -1, -1 })
 alloPbrShader:send('lovrLightColor', { 1, 1, 1, 1.0 })
 alloPbrShader:send('lovrExposure', 2)
+alloPbrShader:send('lovrSphericalHarmonics', sphericalHarmonics)
 alloPbrShader:send('lovrEnvironmentMap', environmentMap)
 
 return alloPbrShader
