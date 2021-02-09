d499468fff9b7f627bd2d25e1e3ee13c3a7aaa9b
Nevyn Bengtsson <nevyn@alloverse.com>
2021-02-09 09:57:17 +0100

    connect to url with text field

--- lua/alloapps/menu/connect_pane.lua
+++ lua/alloapps/menu/connect_pane.lua
@@ -4,21 +4,68 @@ local class = require("pl.class")
 
 class.ConnectPane(ui.Surface)
 function ConnectPane:_init(menu)
-    self:super(ui.Bounds{size=ui.Size(1.6, 1.2, 0.1)})
+    self.menu = menu
+    self:super(ui.Bounds{size=ui.Size(1.6, 1.8, 0.1)})
     self:setColor({1,1,1,1})
 
+    local pen = self.bounds:copy()
+    pen.size.width = pen.size.width - 0.2
+    pen.size.height = 0.2
+    pen:move(0, self.bounds.size.height/2 - 0.1, 0.05)
+
+    local fieldHeaderLabel = self:addSubview(ui.Label{
+        bounds= pen:copy():inset(0, 0.12, 0):move(0, 0, 0),
+        text= "URL to place:",
+        color={0,0,0,1},
+        halign="left"
+    })
+
+    pen:move(0, -pen.size.height, 0)
+    local placeField = self:addSubview(ui.TextField(
+        pen:copy():inset(0, 0.04, 0)
+    ))
+    placeField.onReturn = function(field, text)
+        self:connect(text)
+    end
+
+    pen:move(0, -pen.size.height*1.2, 0)
+    local connectButton = self:addSubview(ui.Button(pen:copy()))
+    connectButton.label.text = "Connect"
+    connectButton.onActivated = function()
+        self:connect(placeField.label.text)
+    end
+
+    pen:move(0 , -pen.size.height - 0.1, 0)
+    local headerLabel = self:addSubview(ui.Label{
+        bounds= pen:copy():inset(0, 0.12, 0):move(0, 0, 0),
+        text= "Recent Places:",
+        color={0,0,0,1},
+        halign="left"
+    })
+
+    pen:move(0, -pen.size.height, 0)
     local connectionList = {
         {"Nevyn's place", "alloplace://nevyn.places.alloverse.com"},
         {"R4", "alloplace://r4.nevyn.nu"},
         {"Localhost", "alloplace://localhost"},
     }
-
-    for i, conn in ipairs(connectionList) do  
-        local connectButton = ui.Button(ui.Bounds(0, 0.7 - i*0.3, 0.01,   1.4, 0.2, 0.15))
+    for i, conn in ipairs(connectionList) do
+        local connectButton = self:addSubview(ui.Button(pen:copy()))
         connectButton.label.text = conn[1]
-        connectButton.onActivated = function() menu:actuate({"connect", conn[2]}) end
-        self:addSubview(connectButton)
+        connectButton.onActivated = function() self:connect(conn[2]) end
+        pen:move(0, -pen.size.height*1.2, 0)
     end
+end
+
+function ConnectPane:connect(urlOrName)
+    local isComplete = string.find(urlOrName, "\\.") ~= nil or urlOrName == "localhost"
+    if not isComplete then
+        urlOrName = urlOrName .. ".places.alloverse.com"
+    end
+    if string.find("alloplace://", urlOrName) == nil then
+        urlOrName = "alloplace://" .. urlOrName
+    end
+    self.menu:actuate({"connect", urlOrName})
 end
 
 return ConnectPane
