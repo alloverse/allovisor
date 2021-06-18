421c5aabb884c66e2c62e0605bf684f1c7d569a6
Nevyn Bengtsson <nevyn@alloverse.com>
2021-06-18 12:02:44 +0200

    only rename things that have the wrong version number

--- rename-builds.sh
+++ rename-builds.sh
@@ -2,7 +2,7 @@
 set -e
 
 VERSION=`cat build/include/allovisor_version.txt | xargs echo -n`
-INSTALLERFILE=`find build -name "*.exe" -or -name "*.dmg" -or -name "*.apk"`
+INSTALLERFILE=`find build -name "*0.2.0*.exe" -or -name "*0.2.0*.dmg" -or -name "*0.2.0*.apk"`
 
 if [ -z "$INSTALLERFILE" ];
 then
