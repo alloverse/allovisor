1999daa069a5851fe583b7c5bef4f32c2960614a
Nevyn Bengtsson <nevyn@alloverse.com>
2022-02-05 19:34:17 +0100

    make text_eng more readable

--- lua/eng/text_eng.lua
+++ lua/eng/text_eng.lua
@@ -55,86 +55,7 @@ function TextEng:onDraw()
   for eid, entity in pairs(self.client.state.entities) do
     local text = entity.components.text
     if text ~= nil then
-      local mat = self.parent.engines.graphics.materials_for_eids[eid]
-      if mat then
-        lovr.graphics.setColor(mat:getColor())
-      else
-        lovr.graphics.setColor(1,1,1,1)
-      end
-      lovr.graphics.push()
-      lovr.graphics.transform(entity.components.transform:getMatrix())
-
-      -- sets a dynamic text scale that fits within a width, if such parameter has been set
-      local dynamicTextScale = 0
-
-      if text.fitToWidth then
-
-        -- backwards compatibility
-        if type(text.fitToWidth) == "number" then
-          text.width = text.fitToWidth
-          text.fitToWidth = true
-        end
-
-        local textLabelWidth = lovr.graphics.getFont():getWidth(text.string)
-        dynamicTextScale = text.width / textLabelWidth  
-      else
-        -- Fit text size to the element's height instead
-        dynamicTextScale = text.height and text.height or 1.0
-      end
-
-      -- old implementation:
-      -- local wrap = text.wrap and text.wrap / (text.height and text.height or 1) or 0
-
-      local wrap = 0
-      -- only care about setting wrap is fitToWidth hasn't been set
-      if not text.fitToWidth then
-        if text.wrap then
-          -- backwards compatibility
-          if type(text.wrap) == "number" then
-            text.width = text.wrap
-            text.wrap = true
-          end
-
-          wrap = text.width and text.width / (text.height and text.height or 1) or 0
-        end
-      end
-
-      if text.halign == "left" then
-        lovr.graphics.translate(-text.width/2,0,0)
-      elseif text.halign == "right" then
-        lovr.graphics.translate(text.width/2,0,0)
-      end
-
-      -- Make sure the text never overflows the height of the container (unless it's wrapped, which is fine.)
-      if dynamicTextScale > text.height then
-        dynamicTextScale = text.height
-      end
-
-
-      lovr.graphics.print(
-        text.string,
-        0, 0, 0.01,
-        dynamicTextScale, --text.height and text.height or 1.0, 
-        0, 0, 0, 0,
-        wrap,
-        text.halign and text.halign or "center",
-        text.valign and text.valign or "middle"
-      )
-
-      if text.insertionMarker then
-        lovr.graphics.setColor(0, 0, 0, math.sin(lovr.timer.getTime()*5)*0.5 + 0.6)
-        local actualLabelWidth, lines = lovr.graphics.getFont():getWidth(text.string, wrap)
-        actualLabelWidth = actualLabelWidth * dynamicTextScale
-        local lastLine = string.match(text.string, "[^%c]*$")
-        local lastLineWidth = lovr.graphics.getFont():getWidth(lastLine) * dynamicTextScale
-        local height = self.font:getHeight()*dynamicTextScale
-        lovr.graphics.line(
-          lastLineWidth + 0.01, height/2 - height*(lines-1), 0,
-          lastLineWidth + 0.01, height/2 - height*lines, 0
-        )
-      end
-
-      lovr.graphics.pop()
+      self:drawText(eid, entity, text)
     end
   end
 
@@ -144,6 +65,89 @@ function TextEng:onDraw()
     lovr.graphics.transform(self.parent.engines.pose:getPose("head"):invert())
   end
   letters.draw()
+
+  lovr.graphics.pop()
+end
+
+function TextEng:drawText(eid, entity, text)
+  local mat = self.parent.engines.graphics.materials_for_eids[eid]
+  if mat then
+    lovr.graphics.setColor(mat:getColor())
+  else
+    lovr.graphics.setColor(1,1,1,1)
+  end
+  lovr.graphics.push()
+  lovr.graphics.transform(entity.components.transform:getMatrix())
+
+  -- sets a dynamic text scale that fits within a width, if such parameter has been set
+  local dynamicTextScale = 0
+
+  if text.fitToWidth then
+
+    -- backwards compatibility
+    if type(text.fitToWidth) == "number" then
+      text.width = text.fitToWidth
+      text.fitToWidth = true
+    end
+
+    local textLabelWidth = lovr.graphics.getFont():getWidth(text.string)
+    dynamicTextScale = text.width / textLabelWidth  
+  else
+    -- Fit text size to the element's height instead
+    dynamicTextScale = text.height and text.height or 1.0
+  end
+
+  -- old implementation:
+  -- local wrap = text.wrap and text.wrap / (text.height and text.height or 1) or 0
+
+  local wrap = 0
+  -- only care about setting wrap is fitToWidth hasn't been set
+  if not text.fitToWidth then
+    if text.wrap then
+      -- backwards compatibility
+      if type(text.wrap) == "number" then
+        text.width = text.wrap
+        text.wrap = true
+      end
+
+      wrap = text.width and text.width / (text.height and text.height or 1) or 0
+    end
+  end
+
+  if text.halign == "left" then
+    lovr.graphics.translate(-text.width/2,0,0)
+  elseif text.halign == "right" then
+    lovr.graphics.translate(text.width/2,0,0)
+  end
+
+  -- Make sure the text never overflows the height of the container (unless it's wrapped, which is fine.)
+  if dynamicTextScale > text.height then
+    dynamicTextScale = text.height
+  end
+
+
+  lovr.graphics.print(
+    text.string,
+    0, 0, 0.01,
+    dynamicTextScale, --text.height and text.height or 1.0, 
+    0, 0, 0, 0,
+    wrap,
+    text.halign and text.halign or "center",
+    text.valign and text.valign or "middle"
+  )
+
+  if text.insertionMarker then
+    lovr.graphics.setColor(0, 0, 0, math.sin(lovr.timer.getTime()*5)*0.5 + 0.6)
+    local actualLabelWidth, lines = lovr.graphics.getFont():getWidth(text.string, wrap)
+    actualLabelWidth = actualLabelWidth * dynamicTextScale
+    local lastLine = string.match(text.string, "[^%c]*$")
+    local lastLineWidth = lovr.graphics.getFont():getWidth(lastLine) * dynamicTextScale
+    local height = self.font:getHeight()*dynamicTextScale
+    lovr.graphics.line(
+      lastLineWidth + 0.01, height/2 - height*(lines-1), 0,
+      lastLineWidth + 0.01, height/2 - height*lines, 0
+    )
+  end
 
   lovr.graphics.pop()
 end
