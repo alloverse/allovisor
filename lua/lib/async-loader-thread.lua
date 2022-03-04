8590bd41e21ae8640551311cc24a379078c7f386
Patrik Sjöberg <voxxar@gmail.com>
2022-03-04 15:15:49 +0100

    loader 4 spaces

--- lua/lib/async-loader-thread.lua
+++ lua/lib/async-loader-thread.lua
@@ -10,35 +10,35 @@ local outChan = lovr.thread.getChannel("AlloLoaderResponses")
 local inChan = lovr.thread.getChannel("AlloLoaderRequests")
 
 function load(type, path, extra, extra2)
-  if type == "model" then
-    -- expects a lovr blob in extra
-    return pcall(lovr.data.newModelData, extra)
-  elseif type == "texture" or type == "image" then
-    -- expects a lovr blob in extra
-    -- expects a boolean 'flip' in extra2
-    return pcall(lovr.data.newImage, extra, not extra2)
-  elseif type == "sound" then
-    -- expects a lovr blob in extra
-    return pcall(lovr.data.newSound, extra)
-  else
-    return false, "no such loader"
-  end
+    if type == "model" then
+        -- expects a lovr blob in extra
+        return pcall(lovr.data.newModelData, extra)
+    elseif type == "texture" or type == "image" then
+        -- expects a lovr blob in extra
+        -- expects a boolean 'flip' in extra2
+        return pcall(lovr.data.newImage, extra, not extra2)
+    elseif type == "sound" then
+        -- expects a lovr blob in extra
+        return pcall(lovr.data.newSound, extra)
+    else
+        return false, "no such loader"
+    end
 end
 
 local running = true
 while running do
-  local type = inChan:pop(true)
-  if type == "quit" then
-    running = false
-  else 
-    local path = inChan:pop(true)
-    local extra = inChan:pop(true)
-    local extra2 = inChan:pop(true)
-    local status, thing = load(type, path, extra, extra2)
-    outChan:push(type)
-    outChan:push(path)
-    outChan:push(status)
-    outChan:push(thing)
-  end
+    local type = inChan:pop(true)
+    if type == "quit" then
+        running = false
+    else 
+        local path = inChan:pop(true)
+        local extra = inChan:pop(true)
+        local extra2 = inChan:pop(true)
+        local status, thing = load(type, path, extra, extra2)
+        outChan:push(type)
+        outChan:push(path)
+        outChan:push(status)
+        outChan:push(thing)
+    end
 end
 print("Exiting loader thread.")
--- lua/lib/async-loader.lua
+++ lua/lib/async-loader.lua
@@ -1,6 +1,6 @@
 -- Request = {callback: Callback}
 local AsyncLoader = {
-  requests = {}, -- {path: String -> req: Request}
+    requests = {}, -- {path: String -> req: Request}
 }
 
 local thread = lovr.thread.newThread("lib/async-loader-thread.lua")
@@ -13,37 +13,37 @@ local inChan = lovr.thread.getChannel("AlloLoaderResponses")
 -- Callback = callback(data: {ErrorString|data}, status: bool) -> Void
 -- AsyncLoader:load(type: Type, path: string, callback: Callback) -> Void
 function AsyncLoader:load(type, path, callback, extra, extra2)
-  local key = type .. "-" .. path
-  local req = self.requests[key]
-  if req then 
-    table.insert(req.callbacks, callback)
-    return
-  end
-  self.requests[key] = {callbacks = {callback}, type = type}
-  outChan:push(type)
-  outChan:push(path)
-  outChan:push(extra)
-  outChan:push(extra2)
+    local key = type .. "-" .. path
+    local req = self.requests[key]
+    if req then 
+        table.insert(req.callbacks, callback)
+        return
+    end
+    self.requests[key] = {callbacks = {callback}, type = type}
+    outChan:push(type)
+    outChan:push(path)
+    outChan:push(extra)
+    outChan:push(extra2)
 end
 
 function AsyncLoader:poll()
-  local type = inChan:pop()
-  local path = inChan:pop()
-  if path == nil then return end
-  local status = inChan:pop(true)
-  local dataOrError = inChan:pop(true)
-  local key = type .. "-" .. path
-  local req = self.requests[key]
-  self.requests[key] = nil
-  for i,callback in ipairs(req.callbacks) do
-    callback(dataOrError, status)
-  end
+    local type = inChan:pop()
+    local path = inChan:pop()
+    if path == nil then return end
+    local status = inChan:pop(true)
+    local dataOrError = inChan:pop(true)
+    local key = type .. "-" .. path
+    local req = self.requests[key]
+    self.requests[key] = nil
+    for i,callback in ipairs(req.callbacks) do
+        callback(dataOrError, status)
+    end
 end
 
 function AsyncLoader:shutdown()
-  print("Shutting down loader...")
-  outChan:push("quit")
-  thread:wait()
+    print("Shutting down loader...")
+    outChan:push("quit")
+    thread:wait()
 end
 
 return AsyncLoader
