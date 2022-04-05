8dd47ad2158e08e95474159a70f8c59fc57c5f11
Patrik Sjöberg <voxxar@gmail.com>
2022-04-05 13:36:48 +0200

    lua gc is suddenly top in profiler

--- lua/main.lua
+++ lua/main.lua
@@ -416,8 +416,6 @@ function lovr.run()
             lovr.math.drain()
         end
 
-        collectgarbage("collect")
-
         if frameLimit > 0 then
             local frameTime = lovr.timer.getTime() - startTime
             while frameTime < 1/frameLimit do
